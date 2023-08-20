9b655d595088b50fdfcbed33d6ca73fd979ce608
mahes <maheshdvp999@gmail.com>
2023-08-20 16:58:38 +0530

    roboshop scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,32 +1,32 @@
 
-echo ">>>>>>>Create  catalogue service file<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Create  catalogue service file<<<<<<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
-echo ">>>>>>>Create  Mongodb Repo<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Create  Mongodb Repo<<<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo ">>>>>>>Create  Node Js Repos<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Create  Node Js Repos<<<<<<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo ">>>>>>>Install Node js<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Install Node js<<<<<<<<<<"
 yum install nodejs -y
-echo ">>>>>>> ADD Roboshop User<<<<<<<<<<"
+echo -e "e[36m -e  ">>>>>>> ADD Roboshop User<<<<<<<<<<"
 useradd roboshop
-echo ">>>>>>>Create  Application Directory<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Create  Application Directory<<<<<<<<<<"
 mkdir /app
-echo ">>>>>>>Download Application content<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Download Application content<<<<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-echo ">>>>>>>Extract Application content<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Extract Application content<<<<<<<<<<"
 
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
-echo ">>>>>>>Download NodeJs Dependencies<<<<<<<<<<"
+echo -e "e[36m -e  ">>>>>>>Download NodeJs Dependencies<<<<<<<<<<"
 
 npm install
-echo ">>>>>>>Install Mongodb Client<<<<<<<<<<"
+echo -e" "e[36m -e ">>>>>>>Install Mongodb Client<<<<<<<<<<"
 
 yum install mongodb-org-shell -y
-echo ">>>>>>>Load  catalogue schema<<<<<<<<<<"
+echo -e "e[36m -e ">>>>>>>Load  catalogue schema<<<<<<<<<<"
 mongo --host mongodb.devops999.store </app/schema/catalogue.js
-echo ">>>>>>>Start  catalogue service <<<<<<<<<<"
+echo -e  "e[36m -e ">>>>>>>Start  catalogue service <<<<<<<<<<"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
