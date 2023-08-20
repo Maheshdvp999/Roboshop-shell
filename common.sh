415715e699bc0dfb0acbe5af23c25250c317e81a
mahes <maheshdvp999@gmail.com>
2023-08-20 18:47:08 +0530

    roboshop scripts

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 nodejs (){
   log=/tmp/roboshop.log
-  echo -e "\e[36m>>>>>>>Create  user service file<<<<<<<<<<\e[0m"
+  echo -e "\e[36m>>>>>>>Create  ${component} service file<<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
   echo -e "\e[36m>>>>>>>Create  Mongodb Repo<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
@@ -8,7 +8,7 @@ nodejs (){
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
   echo -e "\e[36m>>>>>>>Install Node js<<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
-  echo -e "\e[36m>>>>>>> ADD Roboshop User<<<<<<<<<<\e[0m"
+  echo -e "\e[36m>>>>>>> ADD Roboshop ${component}<<<<<<<<<<\e[0m"
   useradd roboshop &>>${log}
   echo -e "\e[36m>>>>>>>Create  Application Directory<<<<<<<<<<\e[0m"
   mkdir /app &>>${log}
@@ -16,9 +16,9 @@ nodejs (){
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &&>>${log}
   echo -e "\e[36m>>>>>>>Extract Application content<<<<<<<<<<\e[0m"
 
-  cd /app &>>${log}
+  cd /app
   unzip /tmp/${component}.zip &>>${log}
-  cd /app &>>${log}
+  cd /app
   echo -e "\e[36m>>>>>>>Download NodeJs Dependencies<<<<<<<<<<\e[0m"
 
   npm install &>>${log}
@@ -26,7 +26,7 @@ nodejs (){
 
   yum install mongodb-org-shell -y &>>${log}
   echo -e "\e[36m>>>>>>>Load user schema<<<<<<<<<<\e[0m"
-  mongo --host mongodb.devops999.store </app/schema/${component}.js
+  mongo --host mongodb.devops999.store </app/schema/user.js
   echo -e "\e[36m>>>>>>>Start user service <<<<<<<<<<\e[0m"
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
