67d34fd8145a182d4af627dfa83602a5f6bba4a0
mahes <maheshdvp999@gmail.com>
2023-08-22 18:15:09 +0530

    conditions

--- common.sh
+++ common.sh
@@ -27,66 +27,30 @@ func_nodejs () {
   log=/tmp/roboshop.log
   echo -e "\e[36m>>>>>>>Create  ${component} service file<<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-if [$? -e0]; then
+  if [$? -e0]; then
  
  echo -e "\e[32m success \e[0m"
-else
-  echo -e "\e[31m Failure \e[0m"
-fi
   echo -e "\e[36m>>>>>>>Create  Mongodb Repo<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-if [$? -eq0]; then
- 
- echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m Failure \e[0m"
-fi
+  echo $?
   echo -e "\e[36m>>>>>>>Create  Node Js Repos<<<<<<<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-if [$? -eq0]; then
- echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m Failure \e[0m"
-fi
+  echo $?
   echo -e "\e[36m>>>>>>>Install Node js<<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
-  if [$? -eq0]; then
- 
- echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m Failure \e[0m"
-fi
+  echo $?
   func_apppreq
-if [$? -eq0]; then
- echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m Failure \e[0m"
-fi
+  echo $?
   echo -e "\e[36m>>>>>>>Download NodeJs Dependencies<<<<<<<<<<\e[0m"
 
   npm install &>>${log}
-if [$? -eq0]; then
- 
- echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m Failure \e[0m"
-fi
+  echo $?
   echo -e "\e[36m>>>>>>>Install Mongodb Client<<<<<<<<<<\e[0m"
 
   func_schema_setup
-  if [$? -eq0]; then
- 
- echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m Failure \e[0m"
-fi
+  echo $?
   func_systemd
-  if [$? -eq0]; then
- 
- echo -e "\e[32m success \e[0m"
-else 
-  echo -e "\e[31m Failure \e[0m"
-fi
+  echo $?
 }
 
 func_java () {
