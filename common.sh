80170794e9d767dc461736d285d304a289266f05
mahes <maheshdvp999@gmail.com>
2023-08-23 19:49:01 +0530

    conditions

--- common.sh
+++ common.sh
@@ -13,23 +13,23 @@ func_apppreq() {
     echo $?
     echo -e "\e[36m>>>>>>> ADD Roboshop ${component}<<<<<<<<<<\e[0m"
     useradd roboshop &>>${log}
-    #func_exit_status
+    func_exit_status
     echo -e "\e[36m>>>>>>>Clean up Applicationcontent<<<<<<<<<<\e[0m"
     rm -rf /app &>>${log}
-    #func_exit_status
+    func_exit_status
     echo -e "\e[36m>>>>>>>Create  Application Directory<<<<<<<<<<\e[0m"
     mkdir /app &>>${log}
 
-    #func_exit_status
+    func_exit_status
     echo -e "\e[36m>>>>>>>Download Application content<<<<<<<<<<\e[0m"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-    #func_exit_status
+    func_exit_status
     echo -e "\e[36m>>>>>>>Extract Application content<<<<<<<<<<\e[0m"
 
     cd /app
     unzip /tmp/${component}.zip &>>${log}
     cd /app
-    #func_exit_status
+    func_exit_status
 }
 
 func_systemd () {
@@ -46,21 +46,21 @@ func_nodejs () {
   func_exit_status
 
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  #func_exit_status
+  func_exit_status
   echo -e "\e[36m>>>>>>>Create  Node Js Repos<<<<<<<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
   func_exit_status
   echo -e "\e[36m>>>>>>>Install Node js<<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
-  #func_exit_status
+  func_exit_status
   func_apppreq
-  #func_exit_status
+  func_exit_status
   echo -e "\e[36m>>>>>>>Download NodeJs Dependencies<<<<<<<<<<\e[0m"
 
   npm install &>>${log}
-  #func_exit_status
+  func_exit_status
   echo -e "\e[36m>>>>>>>Install Mongodb Client<<<<<<<<<<\e[0m"
-  #func_exit_status
+  func_exit_status
   func_schema_setup
 
   func_systemd
@@ -93,19 +93,19 @@ func_schema_setup () {
  if [ "${schema_type}" == "mongodb" ]; then
    echo -e "\e[36m>>>>>>>Install Mongo Client <<<<<<<<<<\e[0m"
    yum install mongodb-org-shell -y &>>${log}
-   #func_exit_status
+   func_exit_status
    echo -e "\e[36m>>>>>>>Load user schema<<<<<<<<<<\e[0m"
    mongo --host mongodb.devops999.store </app/schema/${component}.js &>>${log}
-   #func_exit_status
+   func_exit_status
  fi
 
  if [ "${schema_type}" == "mysql" ]; then
    echo -e "\e[36m>>>>>>>Install MySQL Client <<<<<<<<<<\e[0m"
    yum install mysql -y &>>${log}
-   #func_exit_status
+   func_exit_status
    echo -e "\e[36m>>>>>>>Install Load Schema <<<<<<<<<<\e[0m"
    mysql -h mysql.devops999.store -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>${log}
-   #func_exit_status
+   func_exit_status
 
  fi
 }
