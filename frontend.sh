689677914666a32a7ec0bd3d6d3251f965d22ec8
mahes <maheshdvp999@gmail.com>
2023-08-23 19:59:34 +0530

    conditions

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,27 @@
-
+source common.sh
 echo -e "\e[36m>>>>>>>>>install nginx <<<<<<<<<\e[0m"
 yum install nginx -y
-#func_exit_status
-echo -e "\e[36m>>>>>>>>>install nginx <<<<<<<<<\e[0m"
+func_exit_status
+echo -e "\e[36m>>>>>>>>>copy roboshop configuration <<<<<<<<<\e[0m"
 
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-#func_exit_status
+func_exit_status
 
 echo -e "\e[36m>>>>>>>>>Remove nginx Content<<<<<<<<<\e[0m"
 
 rm -rf /usr/share/nginx/html/*
-#func_exit_status
-echo -e "\e[36m>>>>>>>>>Add  nginx Content<<<<<<<<<\e[0m"
+func_exit_status
+echo -e "\e[36m>>>>>>>>>Download Application Content<<<<<<<<<\e[0m"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+func_exit_status
 cd /usr/share/nginx/html
+echo -e "\e[36m>>>>>>>>>Extract Application Content<<<<<<<<<\e[0m"
+
 unzip /tmp/frontend.zip
-#func_exit_status
+func_exit_status
 echo -e "\e[36m>>>>>>>>>Start nginx <<<<<<<<<\e[0m"
 
 systemctl enable nginx
 systemctl restart nginx
-#func_exit_status
+func_exit_status
