b5722ac0f2be6ad89b665c9ecea77034e3badd76
mahes <maheshdvp999@gmail.com>
2023-08-23 20:26:59 +0530

    conditions

--- payment.sh
+++ payment.sh
@@ -1,3 +1,10 @@
 component=payment
 source common.sh
+
+rabbitmq_app_password =$1
+if [ -z "${rabbitmq_app_password} " ]; then
+  echo Input Rabbitmq AppUser Password Missing
+  exit 1
+fi
+
 func_python
